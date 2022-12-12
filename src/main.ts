22da2e12cc1a976c26afd720446d34e7faa263d3
blck-snwmn <whitesnowmancreator@gmail.com>
2022-12-13 01:35:08 +0900

    Add serialize function

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+function serialize() {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = spreadsheet.getSheetByName('master')
+    if (sheet === null){
+        console.log("failed: sheet(name is 'master') is not found")
+        return
+    }
+    const x = sheet.getRange('A:C')
+    const v = x.getValues().map(v => v.map(String))
+    const [jsonkeys, ...values] = v // head(jsonkeys) is json key
+    let js : Record<string, string>[] = []; 
+    for (const vs of values) {
+        let j : Record<string, string> = {}
+        if (vs[0] === '') {
+            break
+        }
+        jsonkeys.forEach((v, i) => {
+            j[v] = vs[i]
+        })
+        js.push(j)
+    }
+    const json = JSON.stringify(js)
+    console.log(json)
+}
+
+function onOpen(){
+    const sheet = SpreadsheetApp.getActiveSpreadsheet();
+    sheet.addMenu("メニュー", [{name: "json", functionName: "serialize"}]);
+}
